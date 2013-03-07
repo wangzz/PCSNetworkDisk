7080920955e299a751c7cb8fedb91d2003b0d507
wangzz <wangzzvictory_tjsd@163.com>
2013-03-07 22:01:36 +0800

    Revert "Revert "基本工程文件提交""
    This reverts commit facba627371b32cbe9e5e719b4b7c9b67a6ab1df.

---
+++ PCSNetworkDisk/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  PCSNetworkDisk
+//
+//  Created by wangzz on 13-3-7.
+//  Copyright (c) 2013年 hisunsray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ PCSNetworkDisk/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PCSNetworkDisk
+//
+//  Created by wangzz on 13-3-7.
+//  Copyright (c) 2013年 hisunsray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
