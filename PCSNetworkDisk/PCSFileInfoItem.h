f97612b1c23500ed0e3c51909f712d03ca7c49a3
wangzz <wangzzvictory_tjsd@163.com>
2013-03-14 22:58:57 +0800

    修改描述文件的类PCSFileInfoItem属性

--- PCSNetworkDisk/PCSFileInfoItem.h
+++ PCSNetworkDisk/PCSFileInfoItem.h
@@ -10,16 +10,16 @@
 
 @interface PCSFileInfoItem : NSObject
 
-@property (nonatomic, assign) NSInteger  sid;//服务器返回的id
+@property (nonatomic, assign) NSInteger  fid;//文件入库时保存生成的id
 @property (nonatomic, retain) NSString  *name;//文件、文件夹的名字
 @property (nonatomic, retain) NSString  *serverPath;//文件、文件夹在服务器上的路径
-@property (nonatomic, retain) NSString  *localPath;//文件在本地缓存的路径，只有islocal字段为真时才有效
+@property (nonatomic, retain) NSString  *localPath;//文件在本地缓存的路径，只有hasCache字段为真时才有效
 @property (nonatomic, assign) NSInteger size;//文件大小（单位字节）
-@property (nonatomic, assign) PCSFileType  type;//文件、文件夹类型
+@property (nonatomic, assign) PCSFileFormat  format;//文件类型（文件夹，视频，图片等）
+@property (nonatomic, assign) PCSFileProperty   property;//文件属性（删除，上传等）
 @property (nonatomic, assign) BOOL  hasSubFolder;//文件、文件夹是否有子目录
-@property (nonatomic, assign) BOOL  deleted;//文件、文件夹是否被删除
+@property (nonatomic, assign) BOOL  hasCache;//是否有缓存文件
 @property (nonatomic, assign) NSInteger  ctime;//文件、文件夹创建时间
 @property (nonatomic, assign) NSInteger  mtime;//文件、文件夹最近一次修改时间
-@property (nonatomic, assign) BOOL hasCache;//文件在本地是否有缓存
 
 @end
--- PCSNetworkDisk/PCSFileInfoItem.m
+++ PCSNetworkDisk/PCSFileInfoItem.m
@@ -8,22 +8,24 @@
 
 #import "PCSFileInfoItem.h"
 
+
 @implementation PCSFileInfoItem
-@synthesize sid;
+@synthesize fid;
 @synthesize name;
 @synthesize serverPath;
 @synthesize localPath;
 @synthesize size;
-@synthesize type;
+@synthesize format;
 @synthesize hasSubFolder;
-@synthesize deleted;
+@synthesize hasCache;
+@synthesize property;
 @synthesize ctime;
 @synthesize mtime;
-@synthesize hasCache;
+
 
 - (NSString *)description
 {
-    NSString *des = [NSString stringWithFormat:@"sid:%d,name:%@,serverPath:%@,localPath:%@,size:%d,type:%d,hasSubFolder:%d,deleted:%d,ctime:%d,mtime:%d,hasCache:%d",self.sid,self.name,self.serverPath,self.localPath,self.size,self.type,self.hasSubFolder,self.deleted,self.ctime,self.mtime,self.hasCache];
+    NSString *des = [NSString stringWithFormat:@"fid:%d,name:%@,serverPath:%@,localPath:%@,size:%d,format:%d,hasSubFolder:%d,hasCache:%d,property:%d,ctime:%d,mtime:%d,",self.fid,self.name,self.serverPath,self.localPath,self.size,self.format,self.hasSubFolder,self.hasCache,self.property,self.ctime,self.mtime];
     return des;
 }
 
