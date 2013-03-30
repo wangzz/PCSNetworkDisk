92de926091922021acc9db62a25a768c4ee67753
wangzz <wangzzvictory_tjsd@163.com>
2013-03-31 00:37:10 +0800

    更多界面初步效果

--- PCSNetworkDisk/PCSMoreViewController.m
+++ PCSNetworkDisk/PCSMoreViewController.m
@@ -10,10 +10,11 @@
 #import "PCSRootViewController.h"
 
 @interface PCSMoreViewController ()
-
+@property(nonatomic,retain) IBOutlet    UITableView *mTableView;
 @end
 
 @implementation PCSMoreViewController
+@synthesize mTableView;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -29,6 +30,14 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    UIButton    *logoffButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 320, 40)];
+    logoffButton.backgroundColor = [UIColor redColor];
+    [logoffButton setTitle:@"注销登录" forState:UIControlStateNormal];
+    [logoffButton addTarget:self
+                     action:@selector(onLogoffButtonAction)
+           forControlEvents:UIControlEventTouchUpInside];
+    self.mTableView.tableFooterView = logoffButton;
 }
 
 - (void)didReceiveMemoryWarning
@@ -38,10 +47,61 @@
 }
 
 #pragma - 按钮响应事件
-- (IBAction)onLogoutButtonAction:(id)sender
+- (void)onLogoffButtonAction
 {
     [((AppDelegate *)[[UIApplication sharedApplication] delegate]).viewController showViewControllerWith:PCSControllerStateLogin];
     [[NSUserDefaults standardUserDefaults] setBool:NO forKey:PCS_STRING_IS_LOGIN];
+}
+
+#pragma mark- UITableView DataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView
+ numberOfRowsInSection:(NSInteger)section
+{
+    return 3;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView
+heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 48;
+}
+
+- (NSInteger) tableView:(UITableView *)tableView
+indentationLevelForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellid = @"MoreViewControllerCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellid];
+    if (nil == cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                      reuseIdentifier:cellid];
+        cell.selectionStyle = UITableViewCellSelectionStyleBlue;
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [self goToAppStoreGiveAMark];
+}
+
+-(void)goToAppStoreGiveAMark
+{
+    NSString *str = [NSString stringWithFormat:
+                     @"itms-apps://ax.itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=%d",547203890];
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:str]];
 }
 
 
